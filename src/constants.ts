93cc3370b810e3c7c866d8eefe1d2d27dd65a32a
Blake Duncan <blakecduncan@gmail.com>
2024-01-05 14:18:31 -0500

    Fix build issue

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const PRIVY_APP_ID = process.env.REACT_APP_PRIVY_APP_ID
-export const ZERODEV_PROJECT_IDS = [process.env.REACT_APP_ZERODEV_PROJECT_ID]
+export const PRIVY_APP_ID = process.env.REACT_APP_PRIVY_APP_ID ?? ''
+export const ZERODEV_PROJECT_IDS = [process.env.REACT_APP_ZERODEV_PROJECT_ID ?? '']
